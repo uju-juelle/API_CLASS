7524049d044004a15aac574c19389ede2e12b1a3
ujunwa <ujmarylee@gmail.com>
2023-05-09 21:38:54 -0700

    initial

--- Api_app/serializers.py
+++ Api_app/serializers.py
@@ -6,4 +6,4 @@ class PostSerialiizers(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = "__all__"
-        # fields = ["title", "content", "author", "date_posted"]
+        fields = ["title", "content", "author", "date_posted"]
--- Api_app/urls.py
+++ Api_app/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("<int:id>/detail/", api_detailedpage, name="detail"),
     path("<int:id>/update/", api_updatepage, name="update"),
     path("<int:id>/delete/", api_deletepage, name="delete"),
-    path("create/", api_createpage, name="delete"),   
+    path("create/", api_createpage, name="delete")   
 ]
